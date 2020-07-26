8e2634899ca708f7029104943aa72992a10bef6c
braydenlong <longbrayden@gmail.com>
2020-07-26 18:22:08 +0800

    .

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import re
 import telepot
+from flask import Flask
 import os
 PORT = int(os.environ.get('PORT', 5000))
 # import daemon #supposed to run the bot without running the file
